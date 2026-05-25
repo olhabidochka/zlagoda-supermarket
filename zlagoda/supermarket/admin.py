91a9466854d071153b30257120610e56c31e4a1c
olhabidochka <o.bidochka@ukma.edu.ua>
2026-05-25 17:55:47 +0300

    Fixed logout problems

--- zlagoda/supermarket/admin.py
+++ zlagoda/supermarket/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Employee, Category, Product, Store_Product, Customer_Card, Chek, Sale
 
-# Register your models here.
+admin.site.register(Employee)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Store_Product)
+admin.site.register(Customer_Card)
+admin.site.register(Chek)
+admin.site.register(Sale)
--- zlagoda/zlagoda/urls.py
+++ zlagoda/zlagoda/urls.py
@@ -3,8 +3,9 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='supermarket/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
     path('', include('supermarket.urls')),
 ]
